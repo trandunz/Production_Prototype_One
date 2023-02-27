cc740f291cd5010502a63a47d579e66dea20fae7
DawnB9999 <dawn06737@gmail.com>
2023-02-27 20:58:25 +1300

    add potion class and health + mana potion

---
+++ Prototype_One/Source/Prototype_One/ManaPotion.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ManaPotion.h"
+
+ManaPotion::ManaPotion()
+{
+	potionType = EPotionType::Mana;
+	amountRestored = 33;
+}
+
+ManaPotion::~ManaPotion()
+{
+}
---
+++ Prototype_One/Source/Prototype_One/Potion.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+
+// enum for potion type
+enum class EPotionType : uint8
+{
+	Health UMETA(DisplayName = "Health"),
+	Mana UMETA(DisplayName = "Mana"),
+};
+
+class PROTOTYPE_ONE_API Potion
+{
+public:
+	int32 amountRestored;
+	EPotionType potionType;
+	Potion();
+	/**
+	 * @brief pass in a pointer to the value you want to increase
+	 * @param _value 
+	 */
+	void Consume(int *_value);
+	~Potion();
+};
