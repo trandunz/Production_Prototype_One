8a1f2e2ff24fd5311f3beed5c01b5d61a01331b9
trandunz <trandunz@gmail.com>
2023-03-02 22:17:14 +1300

    + pause menu widget c++ base class

---
+++ Prototype_One/Source/Prototype_One/Widgets/PauseMenuWidget.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseMenuWidget.generated.h"
+
+
+UCLASS()
+class PROTOTYPE_ONE_API UPauseMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+};
