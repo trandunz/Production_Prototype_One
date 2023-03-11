a7993684a580600328a55ef4768da3b925a6d654
grovesben <benjaminjames.b@gmail.com>
2023-03-11 14:13:28 +1300

    Enemy indicators

--- Prototype_One/Source/Prototype_One/Widgets/InventoryWidget.cpp
+++ Prototype_One/Source/Prototype_One/Widgets/InventoryWidget.cpp
@@ -2,4 +2,24 @@
 
 
 #include "InventoryWidget.h"
+#include "Prototype_One/Components/PlayerInventory.h"
+#include "Prototype_One/Characters/Prototype_OneCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/TextBlock.h"
 
+void UInventoryWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (auto* player = Cast<APrototype_OneCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)))
+	{
+		FText Rabbits = FText::AsCultureInvariant(FString::FromInt(player->PlayerInventory->GetRabbitSpawnCount()));
+		FText Masked = FText::AsCultureInvariant(FString::FromInt(player->PlayerInventory->GetMaskedSpawnCount()));
+		FText Kings = FText::AsCultureInvariant(FString::FromInt(player->PlayerInventory->GetKingSpawnCount()));
+	
+	
+		RabbitCount->SetText(Rabbits);
+		MaskedCount->SetText(Masked);
+		KingCount->SetText(Kings);
+	}
+}
